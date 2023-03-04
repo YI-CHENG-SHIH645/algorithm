2d67c9c0a899e3aeef4c3b677e6e60ef7fe1ca07
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2023-03-05 00:33:53 +0800

    fix bug of maxAreaOfIsland

--- src/leetcode/connectivity/maxAreaOfIsland.cpp
+++ src/leetcode/connectivity/maxAreaOfIsland.cpp
@@ -9,12 +9,12 @@ int maxAreaOfIsland(vector<vector<int>>& grid) {
   UF<int> uf(grid);
   for(int i=0; i<m; ++i) {
     for(int j=0; j<n; ++j) {
-      if(grid[i][j] - '0') {
-        grid[i][j] = '0';  // I didn't take this into account
-        if(i-1 >= 0 && grid[i-1][j] - '0') uf.uni(i*n+j, (i-1)*n+j);
-        if(j+1 < n && grid[i][j+1] - '0') uf.uni(i*n+j, i*n+j+1);
-        if(i+1 < m && grid[i+1][j] - '0') uf.uni(i*n+j, (i+1)*n+j);
-        if(j-1 >= 0 && grid[i][j-1] - '0') uf.uni(i*n+j, i*n+j-1);
+      if(grid[i][j]) {
+        grid[i][j] = 0;  // I didn't take this into account
+        if(i-1 >= 0 && grid[i-1][j]) uf.uni(i*n+j, (i-1)*n+j);
+        if(j+1 < n && grid[i][j+1]) uf.uni(i*n+j, i*n+j+1);
+        if(i+1 < m && grid[i+1][j]) uf.uni(i*n+j, (i+1)*n+j);
+        if(j-1 >= 0 && grid[i][j-1]) uf.uni(i*n+j, i*n+j-1);
       }
     }
   }
