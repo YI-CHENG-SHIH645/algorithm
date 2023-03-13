09465cb83531b3ed740162e62c4b8322fa452be0
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2023-03-13 22:26:16 +0800

    add files

---
+++ src/tests/leetcode/test_tree.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 施奕成 on 2023/3/13.
+//
+#include <gtest/gtest.h>
+#include <tree/tree.h>
+
+TEST(TreeTest, sortedArrayToBST) {
+  std::vector<int> a{1, 2, 3, 4, 5};
+  TreeNode ans = sortedArrayToBST(a);
+  EXPECT_TRUE(isValidBST(ans));
+}
