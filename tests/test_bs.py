a7c1242af5bd1baf448f40aa734d7ff23038f01e
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2023-03-18 03:22:29 +0800

    add files

---
+++ tests/test_bs.py
@@ -0,0 +1,7 @@
+from algorithm.leetcode import bs
+
+
+def test_for_bs():
+    assert bs.findMin([3, 4, 5, 1, 2]) == 1
+    assert bs.findMin([4, 5, 6, 7, 0, 1, 2]) == 0
+    assert bs.findMin([11, 13, 15, 17]) == 11
